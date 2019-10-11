3d72d5cc35e836da91df09a97c8fbdb38ec59981
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-10-11 16:36:12 +0500

    creates a app template and a welcome page

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="center">
+        <h2>Coder's Tape</h2>
+        <p>This is a testing site.</p>
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+@extends('app')
+
+@section('title', "Coder's Tape")
