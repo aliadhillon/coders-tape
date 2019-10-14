581bc327d917f5f6ac1d97e8d4677e60d058d8d4
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-10-14 16:22:22 +0500

    create new customer and some refactoring

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -6,24 +6,24 @@
     <h2>Create New Customer</h2>
     <form action="{{ route('customers.store') }}" method="post">
         @csrf
-        <p>
+        <div class="form-group">
             <label for="name">Name:</label>
             <br>
             <input class="@error('name') invalid @enderror" type="text" name="name" id="name" size="40" autocomplete="off" value="{{ old('name') }}">
             @error('name')
                 <span class="text-red">{{ $message }}</span>
             @enderror
-        </p>
-        <p>
+        </div>
+        <div class="form-group">
             <label for="email">Email:</label>
             <br>
             <input class="@error('name') invalid @enderror" type="email" name="email" id="email" size="40" autocomplete="off" value="{{ old('email') }}">
             @error('email')
                 <span class="text-red">{{ $message }}</span>
             @enderror
-        </p>
-        <p>
+        </div>
+        <div class="form-group">
             <button type="submit">Add New Customer</button>
-        </p>
+        </div>
     </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/services', 'ServiceController@index')->name('services.index');
 Route::post('/services', 'ServiceController@store')->name('services.store');
 
 Route::prefix('customers')->as('customers.')->group(function(){
+
     Route::get('/', 'CustomerController@index')->name('index');
     Route::get('/create', 'CustomerController@create')->name('create');
     Route::post('/', 'CustomerController@store')->name('store');
