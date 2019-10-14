08bb0e7443ee18b4556f3175beb13305b788fdb7
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-10-14 23:20:14 +0500

    Creates CustomersTable Seeder

---
+++ database/seeds/CustomersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Customer;
+use Illuminate\Database\Seeder;
+
+class CustomersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Customer::truncate();
+        
+        factory(Customer::class, 20)->create();
+    }
+}
