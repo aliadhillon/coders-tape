476c8699f37e7bda21fda0c1b534045e6b534ea2
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-10-14 22:41:59 +0500

    Adds method deatils in CustomerController

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,6 +7,11 @@ use App\Http\Requests\CustomerRequest;
 
 class CustomerController extends Controller
 {
+    /**
+     * Show all the Customers list
+     *
+     * @return View
+     */
     public function index()
     {
         $customers = Customer::all();
@@ -14,11 +19,22 @@ class CustomerController extends Controller
         return view('customer.index', compact('customers'));
     }
 
+    /**
+     * Show the form for creating a new Customer
+     *
+     * @return View
+     */
     public function create()
     {
         return view('customer.create');
     }
 
+    /**
+     * Store the New customer with the data coming from the request
+     *
+     * @param CustomerRequest $request
+     * @return Redirect
+     */
     public function store(CustomerRequest $request)
     {
         $customer = Customer::create($request->validated());
@@ -26,16 +42,35 @@ class CustomerController extends Controller
         return redirect()->route('customers.show', compact('customer'));
     }
 
+    /**
+     * Show the Customer details
+     *
+     * @param Customer $customer
+     * @return View
+     */
     public function show(Customer $customer)
     {
         return view('customer.show', compact('customer'));
     }
 
+    /**
+     * Show th edit page for Customer
+     *
+     * @param Customer $customer
+     * @return View
+     */
     public function edit(Customer $customer)
     {
         return view('customer.edit', compact('customer'));
     }
 
+    /**
+     * Update the Gien Customer with the given Request data
+     *
+     * @param CustomerRequest $request
+     * @param Customer $customer
+     * @return Redirect
+     */
     public function update(CustomerRequest $request, Customer $customer)
     {
         $customer->update($request->validated());
@@ -43,6 +78,12 @@ class CustomerController extends Controller
         return redirect()->route('customers.show', compact('customer'))->with('msg', "Customer Updated Successfully");
     }
 
+    /**
+     * Delte the given Customer
+     *
+     * @param Customer $customer
+     * @return Redirect
+     */
     public function destroy(Customer $customer)
     {
         $customer->delete();
