e3f38605e462c40c55de9db196850061b270ea97
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-10-21 21:54:12 +0500

    still playing wiht Storage

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Customer;
 use App\Service;
+use Carbon\Carbon;
+use Illuminate\Http\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -11,6 +13,7 @@ class WelcomeController extends Controller
 {
     public function index()
     {
+        Storage::append('uploads/sample.txt', 'This is the first line');
         return view('welcome');
     }
 
